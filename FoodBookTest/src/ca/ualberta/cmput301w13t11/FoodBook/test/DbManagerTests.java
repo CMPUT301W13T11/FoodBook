f2930ea6df1030069b079b8a2204a2bdf7cb6b09
mbabic <mbabic@ualbeta.ca>
2013-03-10 17:14:12 -0600

    AndroidTestCases working; couple of examples of using reflection to test private methods.

--- FoodBookTest/src/ca/ualberta/cmput301w13t11/FoodBook/test/DbManagerTests.java
+++ FoodBookTest/src/ca/ualberta/cmput301w13t11/FoodBook/test/DbManagerTests.java
@@ -1,10 +1,12 @@
 package ca.ualberta.cmput301w13t11.FoodBook.test;
 
-import org.junit.Assert;
-import org.junit.Test;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
+import android.content.ContentValues;
 import android.test.AndroidTestCase;
 import ca.ualberta.cmput301w13t11.FoodBook.model.DbManager;
+import ca.ualberta.cmput301w13t11.FoodBook.model.Ingredient;
 import ca.ualberta.cmput301w13t11.FoodBook.model.Recipe;
 
 /**
@@ -14,23 +16,93 @@ import ca.ualberta.cmput301w13t11.FoodBook.model.Recipe;
  */
 public class DbManagerTests extends AndroidTestCase {
 
+	private DbManager db = null;
+//	public DbManagerTests()
+//	{
+//		super("ca.ualberta.cmput301w13t11.FoodBook.model", DbManagerTestActivity.class);
+//	}
+	protected void setUp() throws Exception
+	{
+		super.setUp();
+	}	
 
-	private DbManager instance = null;
+	public void testGetInstance()
+	{
+		db = DbManager.getInstance();
+		if (db == null)
+			fail("getInstance failed");
+		assertTrue(true);
+	}
 	
-
 	/*
 	 * Test the getInstance() method -- should not return null.
 	 */
-	public void testWhatever() 
+	public void testInsert() 
 	{
-		Assert.assertTrue(true);
+		Recipe recipe = Recipe.generateTestRecipe();
+		db = DbManager.getInstance();
+		if (db == null)
+			fail();
+		
+		db.insert(recipe);
+		assertTrue(true);
 	}
 	
-
-	public void testInsertRecipe()
+	/*
+	 * Test RecipeToMap() 
+	 */
+	public void testRecipeToMap()
 	{
-		instance = DbManager.getInstance();
-		instance.insert(Recipe.generateTestRecipe());
-		Assert.assertTrue(true);
+		db = DbManager.getInstance();
+		Recipe recipe = Recipe.generateTestRecipe();
+		try {
+			/* Testing private member function, need to use reflection. */
+			Class[] args = new Class[1];
+			args[0] = Recipe.class;
+			Method method = db.getClass().getDeclaredMethod("RecipeToMap", args);
+			method.setAccessible(true);
+			ContentValues cv = (ContentValues) method.invoke(db, recipe);
+			assertTrue(cv != null);
+			
+		} catch (NoSuchMethodException nsme) {
+			fail("NoSuchMethodException");
+		} catch (IllegalArgumentException e) {
+			fail("IllegalArgumentException");
+		} catch (IllegalAccessException e) {
+			fail("IllegalAccessException");
+		} catch (InvocationTargetException e) {
+			fail("InvocationTargetException");
+		}
+		
 	}
+	
+	/*
+	 * Test RecipeToMap() 
+	 */
+	public void testIngredientToMap()
+	{
+		db = DbManager.getInstance();
+		Ingredient ingredient = new Ingredient("test", "test", 100);
+		
+		try {
+			/* Testing private member function, need to use reflection. */
+			Class[] args = new Class[1];
+			args[0] = Ingredient.class;
+			Method method = db.getClass().getDeclaredMethod("IngredientToMap", args);
+			method.setAccessible(true);
+			ContentValues cv = (ContentValues) method.invoke(db, ingredient);
+			assertTrue(cv != null);
+			
+		} catch (NoSuchMethodException nsme) {
+			fail("NoSuchMethodException");
+		} catch (IllegalArgumentException e) {
+			fail("IllegalArgumentException");
+		} catch (IllegalAccessException e) {
+			fail("IllegalAccessException");
+		} catch (InvocationTargetException e) {
+			fail("InvocationTargetException");
+		}
+		
+	}
+
 }
