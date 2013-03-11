8f0ff63aae56245abc18de2d34ed16a1d53956ef
tcline1 <tcline@ualberta.ca>
2013-03-10 20:34:40 -0600

    Fixed Add Recipe

--- FooBook/src/ca/ualberta/cmput301w13t11/FoodBook/EditIngredients.java
+++ FooBook/src/ca/ualberta/cmput301w13t11/FoodBook/EditIngredients.java
@@ -3,7 +3,6 @@ package ca.ualberta.cmput301w13t11.FoodBook;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
-import android.view.Menu;
 import android.view.View;
 
 public class EditIngredients extends Activity
@@ -14,21 +13,13 @@ public class EditIngredients extends Activity
 	{
 
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_edit_photos);
+		setContentView(R.layout.activity_edit_ingredients);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu)
-	{
-
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.edit_photos, menu);
-		return true;
-	}
 	public void OnGotoMainMenu(View View)
     {
 		// responds to button Main Menu
-		 Intent intent = new Intent(View.getContext(), MainScreen.class);
+		 Intent intent = new Intent(View.getContext(), MyRecipes.class);
 		 intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 		 EditIngredients.this.finish();
     }
