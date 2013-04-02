7afebd056dcc1ad08cffcabb0156f679deb6c46c
tupes <tupala@ualberta.ca>
2013-04-02 14:44:05 -0600

    cleaning

--- FooBook/src/ca/ualberta/cmput301w13t11/FoodBook/model/IngredientsDbManager.java
+++ FooBook/src/ca/ualberta/cmput301w13t11/FoodBook/model/IngredientsDbManager.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteException;
 import ca.ualberta.cmput301w13t11.FoodBook.controller.DbController;
 /**
  * Implements the functionality required to manage the Ingredients database, which
@@ -19,8 +20,7 @@ public class IngredientsDbManager extends DbManager {
 	private String tableName = "UserIngredients";
 	private String getSQL = "SELECT * FROM " + tableName;
 
-	public IngredientsDbManager(Context context)
-	{
+	public IngredientsDbManager(Context context) {
 		super(context);
 	}
 
@@ -29,8 +29,7 @@ public class IngredientsDbManager extends DbManager {
 	 * @param context Context needed to execute database creation statements (if necessary).
 	 * @return The instance of IngredientsDbManager.
 	 */
-	public static IngredientsDbManager getInstance(Context context)
-	{
+	public static IngredientsDbManager getInstance(Context context) {
 		// call super to create Db if necessary
 		DbManager.getInstance(context);
 		if (instance == null)
@@ -41,18 +40,24 @@ public class IngredientsDbManager extends DbManager {
 	/**
 	 * @return The instance of the RecipesDbManager.
 	 */
-	public static IngredientsDbManager getInstance()
-	{
+	public static IngredientsDbManager getInstance() {
 		return instance;
 	}
 
 	/**
 	 * Inserts the given Ingredient into the database.
 	 * @param ingred The ingredient to be stored.
+	 * @return True on success, False on failure
 	 */
-	public void insert(Ingredient ingred) {
+	public boolean insert(Ingredient ingred) {
 		ContentValues values = ingred.toContentValues();
-		db.insert(tableName, null, values);
+		try {
+			db.insert(tableName, null, values);
+        } catch (SQLiteException sqle) {
+    		sqle.printStackTrace();
+    		return false;
+    	}
+		return true;
 	}
 
 	/**
@@ -60,41 +65,38 @@ public class IngredientsDbManager extends DbManager {
 	 * @return ArrayList of Ingredients
 	 */
 	public ArrayList<Ingredient> get() {
-		Cursor cursor = db.rawQuery(getSQL, null);
-		return cursorToMyIngredients(cursor);
+		Cursor cursor;
+		try {
+			cursor = db.rawQuery(getSQL, null);
+        } catch (SQLiteException sqle) {
+    		sqle.printStackTrace();
+    		return new ArrayList<Ingredient>();
+    	}
+		return cursorToIngredients(cursor);
 	}
 
 	/**
 	 * Deletes the given Ingredient from the database.
 	 * @param ingred the ingredient to be deleted.
+	 * @return True on success, False on failure
 	 */
-	public void delete(Ingredient ingred) {
-		db.delete(tableName, "name = '" + ingred.getName()+"'", null);
+	public boolean delete(Ingredient ingred) {
+		try {
+			db.delete(tableName, "name = '" + ingred.getName()+"'", null);
+        } catch (SQLiteException sqle) {
+    		sqle.printStackTrace();
+    		return false;
+    	}
+		return true;
 	}
-
-	// *********************************************
-	// UPDATE INGREDIENT
-	// *********************************************
 
 	/**
 	 * Update the entry in the Db with the uri given the Recipe parameter with the values
 	 * given by the Recipe parameter.
 	 * @param recipe The updated recipe to be stored in the database.
+	 * @return True on success, False on failure
 	 */
-//	public void updateIngredient(Ingredient ingred, Ingredient oldIngred) {
-//		updateField(oldIngred, "quantity", Float.toString(ingred.getQuantity()));
-//		updateField(oldIngred, "unit", ingred.getUnit());
-//		updateField(oldIngred, "name", ingred.getName());
-//	}
-
-	/**
-	 * Updates the title of the Ingredient with the given name.
-	 * TODO: consider making this a private part of an "update recipe function"
-	 * @param uri The URI of the Recipe to be updated.
-	 * @param tableName The name of the table in which the Recipe resides.
-	 * @param newTitle The new title of the Recipe.
-	 */
-	public void updateIngredient(Ingredient ingred, Ingredient oldIngred) {
+	public boolean updateIngredient(Ingredient ingred, Ingredient oldIngred) {
 		String filter = "name='" + oldIngred.getName() 
 		        + "' and unit='" + oldIngred.getUnit()
 		        + "' and quantity='" + oldIngred.getQuantity() + "'";
@@ -102,26 +104,13 @@ public class IngredientsDbManager extends DbManager {
 		args.put("name", ingred.getName());
 		args.put("unit", ingred.getUnit());
 		args.put("quantity", ingred.getQuantity());
-		db.update(tableName, args, filter, null);
-	}
-
-	/**
-	 * Converts the cursor, retrieved by querying the MyIngredientsTable, and converts it to an ArrayList of ingredients.
-	 * @param cursor The cursor over which we will iterate to get the ingredients from.
-	 * @return An ArrayList of ingredients.
-	 */
-	private ArrayList<Ingredient> cursorToMyIngredients(Cursor cursor) {
-		ArrayList<Ingredient> ingredients = new ArrayList<Ingredient>();
-		cursor.moveToFirst();
-		while (!cursor.isAfterLast()) {
-			String name = cursor.getString(0);
-			String unit = cursor.getString(1);
-			float quantity = cursor.getFloat(2);
-			Ingredient ingredient = new Ingredient(name, unit, quantity);
-			ingredients.add(ingredient);
-			cursor.moveToNext();
-		}
-		return ingredients;
+		try {
+			db.update(tableName, args, filter, null);
+        } catch (SQLiteException sqle) {
+    		sqle.printStackTrace();
+    		return false;
+    	}
+		return true;
 	}
 
 	public String getTableName() {
